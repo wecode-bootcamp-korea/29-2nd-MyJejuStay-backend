beceef3217418965f85cec0962db236945ea4181
11292021 <xforwalkers@gmail.com>
2022-02-15 16:57:33 +0900

    Add: Initial_Modeling

--- accommodations/models.py
+++ accommodations/models.py
@@ -3,24 +3,27 @@ from django.db import models
 from users.models import Base, User
 
 class Accommodation(Base):
-    name           = models.CharField(max_length=250)
-    description    = models.CharField(max_length=250)
-    price          = models.DecimalField(decimal_places=2, max_digits=10)
-    address        = models.CharField(max_length=250)
-    region         = models.CharField(max_length=30)
-    is_verified    = models.BooleanField(default=False)
-    latitude       = models.DecimalField(decimal_places=7, max_digits=10)
-    longtitude     = models.DecimalField(decimal_places=7, max_digits=10)
-    check_in_time  = models.DateTimeField()
-    check_out_time = models.DateTimeField()
-    minimum_stay   = models.PositiveIntegerField(default=1)
+    name               = models.CharField(max_length=250)
+    description        = models.CharField(max_length=250)
+    price              = models.DecimalField(decimal_places=2, max_digits=10)
+    address            = models.CharField(max_length=250)
+    region             = models.CharField(max_length=30)
+    is_verified        = models.BooleanField(default=False)
+    latitude           = models.DecimalField(decimal_places=7, max_digits=10)
+    longtitude         = models.DecimalField(decimal_places=7, max_digits=10)
+    check_in_time      = models.TimeField()
+    check_out_time     = models.TimeField()
+    minimum_stay       = models.PositiveIntegerField(default=1)
+    detail_description = models.TextField(blank=True)
+    check_in_date      = models.DateTimeField(null=True)
+    check_out_date     = models.DateTimeField(null=True)
 
     class Meta:
         db_table = 'accommodations'
 
 class AccommodationImage(Base):
     accommodation = models.ForeignKey(Accommodation, on_delete=models.CASCADE)
-    image_url     = models.CharField(max_length=250)
+    image_url     = models.CharField(max_length=250, null=True)
 
     class Meta:
         db_table = 'accommodation_images'
