bc8158e32e5f53ecd4a6c8bed32b11f8f47b9720
11292021 <xforwalkers@gmail.com>
2022-02-16 17:37:57 +0900

    Fix: conflict

--- users/models.py
+++ users/models.py
@@ -8,7 +8,6 @@ class Base(models.Model):
         abstract = True
 
 class User(Base):
-<<<<<<< HEAD
     name       = models.CharField(max_length=30)
     email      = models.EmailField(max_length=250)
     password   = models.CharField(max_length=250, null=True)
@@ -17,19 +16,3 @@ class User(Base):
 
     class Meta:
         db_table = 'users'
-=======
-    name                 = models.CharField(max_length=30)
-    email                = models.EmailField(max_length=250)
-    password             = models.CharField(max_length=250, null=True)
-    has_agreed           = models.BooleanField(default=False)
-
-    class Meta:
-        db_table = 'users'
-
-class Social(Base):
-    user                 = models.ForeignKey(User, on_delete=models.CASCADE)
-    social_platform_name = models.CharField(max_length=250) 
-
-    class Meta:
-        db_table = 'socials'
->>>>>>> a3c41a7 (Add: Initial Models)
