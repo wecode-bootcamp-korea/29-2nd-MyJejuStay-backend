8df58b4454cb439f1c61d0317ca34f77d7ea4ad4
cch0807 <https://github.com/cch0807>
2022-02-21 19:32:45 +0900

    Modify: views.py

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import SignUpView, KakaoLoginView
+from .views import SignUpView, KakaoLoginView, LogInView
 
 urlpatterns = [
   path('/kakaologin', KakaoLoginView.as_view()),
-  path('/signup', SignUpView.as_view())
+  path('/signup', SignUpView.as_view()),
+  path('/login', LogInView.as_view())
 ]
--- users/views.py
+++ users/views.py
@@ -14,7 +14,6 @@ import jwt
 import bcrypt
 
 from users.models           import User
-from my_settings            import SECRET_KEY, ALGORITHMS
 from .validate              import validate_email, validate_password
 
 class SignUpView(View):
@@ -77,3 +76,22 @@ class KakaoLoginView(View):
 
     except KeyError:
       return JsonResponse({'message': 'KEY_ERROR'}, status=400)
+
+class LogInView(View):
+  def post(self, request):
+    try:
+      data  = json.loads(request.body) 
+      user  = User.objects.get(email=data['email'])
+      if not bcrypt.checkpw(data['password'].encode('utf-8'), user.password.encode('utf-8')):
+                return JsonResponse({'message': 'INVALID_PASSWORD'}, status = 401)
+
+      token = jwt.encode({'id': user.id}, settings.SECRET_KEY, settings.ALGORITHMS)
+      return JsonResponse({'message':'SUCCESS','token':token}, status=201)
+
+    except KeyError:
+      return JsonResponse({'message': 'KEY_ERROR'}, status=400)
+
+    except User.DoesNotExist:
+      return JsonResponse({'message': 'INVALID_EMAIL'}, status=400)
+
+
