c80ae5da954ac732ebff426cc2e403b3e063301c
11292021 <xforwalkers@gmail.com>
2022-02-22 21:31:45 +0900

    Edit: Initial Model

--- reservations/models.py
+++ reservations/models.py
@@ -9,11 +9,12 @@ from accommodations.models import Accommodation
 class Reservation(Base):
     user               = models.ForeignKey(User, on_delete=models.CASCADE)
     accommodation      = models.ForeignKey(Accommodation, on_delete=models.CASCADE)
-    check_in           = models.DateTimeField()
-    check_out          = models.DateTimeField()
+    check_in           = models.DateTimeField(null=True)
+    check_out          = models.DateTimeField(null=True)
     number_of_adults   = models.PositiveIntegerField(default=1)
     number_of_children = models.IntegerField(default=0)
-    reservation_code   = models.UUIDField(default=uuid.uuid4)
+    reservation_code   = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    total_price        = models.DecimalField(default=0, decimal_places=2, max_digits=10)
 
     class Meta:
         db_table = 'reservations'
