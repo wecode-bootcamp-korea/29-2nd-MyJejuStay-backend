28c83b46ed56ecf1cf7a99c5370740e120ba91c0
cch0807 <https://github.com/cch0807>
2022-02-22 10:01:48 +0900

    Add: reservation 정보 반환
    Modify: views.py 수정
    
    Modify: views.py 수정

--- users/views.py
+++ users/views.py
@@ -10,11 +10,16 @@ from django.views           import View
 from django.conf            import settings
 from django.core.exceptions import ValidationError
 
+from accommodations.models import Accommodation
+from reservations.models   import Reservation
+
 import jwt
 import bcrypt
 
 from users.models           import User
 from .validate              import validate_email, validate_password
+
+from users.utils            import login_decorator
 
 class SignUpView(View):
   def post(self,request):
@@ -59,7 +64,7 @@ class KakaoLoginView(View):
       kakao_account = kakao_account_response.json()
       name     = kakao_account['kakao_account']['profile']['nickname'],
       kakao_id = kakao_account['id']
-      email = 'ckdgus1011@gamil.com'
+      email = 'ckdgus1101@naver.com'
       #  email    = kakao_account['kakao_account']['email']
 
       user, is_created = User.objects.get_or_create(
@@ -71,8 +76,7 @@ class KakaoLoginView(View):
       
       access_token = jwt.encode({'user_id' :  kakao_id}, settings.SECRET_KEY, settings.ALGORITHMS)
 
-      print('token',access_token)
-      return JsonResponse({"token":access_token}, status=200)
+      return JsonResponse({"token":access_token, 'message': 'SUCCESS'}, status=200)
 
     except KeyError:
       return JsonResponse({'message': 'KEY_ERROR'}, status=400)
@@ -83,7 +87,7 @@ class LogInView(View):
       data  = json.loads(request.body) 
       user  = User.objects.get(email=data['email'])
       if not bcrypt.checkpw(data['password'].encode('utf-8'), user.password.encode('utf-8')):
-                return JsonResponse({'message': 'INVALID_PASSWORD'}, status = 401)
+        return JsonResponse({'message': 'INVALID_PASSWORD'}, status = 401)
 
       token = jwt.encode({'id': user.id}, settings.SECRET_KEY, settings.ALGORITHMS)
       return JsonResponse({'message':'SUCCESS','token':token}, status=201)
@@ -95,3 +99,24 @@ class LogInView(View):
       return JsonResponse({'message': 'INVALID_EMAIL'}, status=400)
 
 
+class ReservationInfoView(View):
+  @login_decorator
+  def get(self,request):
+    try:
+      reservations = Reservation.objects.filter(user_id = request.user).order_by('-created_at')
+
+      result=[{
+        'user_id'           : reservation.user_id,
+        'accommodation_name': reservation.accommodation.name,
+        'price'             : reservation.total_price,
+        'check_in_date'     : reservation.check_in,
+        'check_out_date'    : reservation.check_out,
+        'number_of_adults'  : reservation.number_of_adults,
+        'number_of_children': reservation.number_of_children,
+        'image_url'         : [image.image_url for image in reservation.accommodation.accommodationimage_set.all()],
+      }for reservation in reservations]
+      
+      return JsonResponse({'result':result},status=200)
+
+    except KeyError:
+      return JsonResponse({'message': 'KEY_ERROR'}, stauts=400)
